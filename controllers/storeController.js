cbfe1efe12cf361db869b7ca8dbc8596f4f99de8
Ola Vea <olavea@Olas-MacBook-Pro.local>
2017-11-22 07:23:39 +0100

    Video 10 og 11. 10: Saving Stores and using Mixins 11: Using Async/ Await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,10 +1,12 @@
+
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
     res.render('index');
 }; 
 
 
-// const mongoose = require('mongoose');
-// const Store = mongoose.model('Store');
 // const multer = require('multer');
 // const jimp = require('jimp');
 // const uuid = require('uuid');
@@ -44,6 +46,10 @@ exports.addStore = (req, res) => {
 // }; 
 
 exports.createStore = async (req, res) => {
+    const store = new Store(req.body);
+    await store.save();
+    res.redirect('/');
+};
 //     const store = await (new Store(req.body)).save();
 //     req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
 //     res.redirect(`/store/${store.slug}`);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
-
+const { catchErrors } = require('../handlers/errorHandlers');
 router.get('/', storeController.homePage);
 
 

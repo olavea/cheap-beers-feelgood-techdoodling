d725e21659111a1cd49a4b9e8f829223aaae9f4b
Ola Vea <olavea@Olas-MacBook-Pro.local>
2017-11-22 17:07:06 +0100

    Video 23 Creating User Accounts. 1. validate the registration data. 2. register the user. 3. We need to log them in.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,37 +1,37 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // const User = mongoose.model('User');
 // const promisify = require('es6-promisify');
 
-// exports.loginForm = (req, res) => {
-//     res.render('login', { title: 'Login' });
-// };
+exports.loginForm = (req, res) => {
+    res.render('login', { title: 'Login' });
+};
 
-// // exports.registerForm = (req, res) => {
-// //     res.render('register', { title: 'Register'}); 
-// // };
+exports.registerForm = (req, res) => {
+    res.render('register', { title: 'Register'}); 
+};
 
-// exports.validateRegister = (req, res, next) => {
-//     req.sanitizeBody('name');
-//     req.checkBody('name', 'You must supply a name!').notEmpty();
+exports.validateRegister = (req, res, next) => {
+    req.sanitizeBody('name');
+    req.checkBody('name', 'You must supply a name!').notEmpty();
 
-//     req.checkBody('email', 'ThatEmail is not valid!').isEmail();
-//     req.sanitizeBody('email').normalizeEmail({ 
-//       remove_dots: false,
-//       remove_extension: false,
-//       gmail_remove_subaddress: false
-//     });
-    // req.checkBody('password', 'Password Cannot be Blank!').notEmpty();
-    // req.checkBody('password-confirm', 'Password Cannot be Blank!').notEmpty();
-    // req.checkBody('password-confirm', 'Oops! Your Password do not match').equals(req.body.password);
+    req.checkBody('email', 'ThatEmail is not valid!').isEmail();
+    req.sanitizeBody('email').normalizeEmail({ 
+      remove_dots: false,
+      remove_extension: false,
+      gmail_remove_subaddress: false
+    });
+    req.checkBody('password', 'Password Cannot be Blank!').notEmpty();
+    req.checkBody('password-confirm', 'Password Cannot be Blank!').notEmpty();
+    req.checkBody('password-confirm', 'Oops! Your Password do not match').equals(req.body.password);
 
-    // const errors = req.validationErrors();
-    // if (errors) {
-    //     req.flash('error', errors.map(err => err.msg ));
-    //     res.render('register', { title: 'Register', body: req.body, flashes: req.flash() });
-//         return; // stop the function from running
-//     }
-//     next(); // There were no errors
-// };
+    const errors = req.validationErrors();
+    if (errors) {
+        req.flash('error', errors.map(err => err.msg));
+        res.render('register', { title: 'Register', body: req.body, flashes: req.flash() });
+        return; // stop the function from running
+    }
+    next(); // There were no errors
+};
 
 // exports.register = async (req, res, next) => {
 //   const user = new User({ email: req.body.email, name: req.body.name });
@@ -40,7 +40,7 @@
   
 //   next(); // pass to authController.login
 
-};
+
 // dog123
 // 9hsajfvlakbhvaødosfijvælakdfnb
 
